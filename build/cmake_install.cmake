748719bdce4bc4864fd7723af8fe989af588a0b6
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2025-01-19 14:05:47 -0300

    build

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/enzo/Imagens/G1-S9-A2
+# Install script for directory: C:/Users/Maiky/Documents/GitHub/g1-s9-a2-embarcatech
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/G1-S9-A2")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/enzo/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump")
+  set(CMAKE_OBJDUMP "C:/Users/Maiky/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/enzo/Imagens/G1-S9-A2/build/pico-sdk/cmake_install.cmake")
+  include("C:/Users/Maiky/Documents/GitHub/g1-s9-a2-embarcatech/build/pico-sdk/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/enzo/Imagens/G1-S9-A2/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Maiky/Documents/GitHub/g1-s9-a2-embarcatech/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- build/pico-sdk/src/rp2_common/pico_fix/cmake_install.cmake
+++ build/pico-sdk/src/rp2_common/pico_fix/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/enzo/.pico-sdk/sdk/2.1.0/src/rp2_common/pico_fix
+# Install script for directory: C:/Users/Maiky/.pico-sdk/sdk/2.1.0/src/rp2_common/pico_fix
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/G1-S9-A2")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,11 +34,11 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/enzo/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump")
+  set(CMAKE_OBJDUMP "C:/Users/Maiky/.pico-sdk/toolchain/13_3_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/enzo/Imagens/G1-S9-A2/build/pico-sdk/src/rp2_common/pico_fix/rp2040_usb_device_enumeration/cmake_install.cmake")
+  include("C:/Users/Maiky/Documents/GitHub/g1-s9-a2-embarcatech/build/pico-sdk/src/rp2_common/pico_fix/rp2040_usb_device_enumeration/cmake_install.cmake")
 endif()
 
